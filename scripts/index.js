4d9b7f6053e82bbafa1a01df397f85c5e0b4d665
Alexander Pierpoint <alexanderpierpoint@Rebeccas-MacBook-Pro-2.local>
2019-08-05 08:12:59 -0700

    Finished Template?

--- scripts/index.js
+++ scripts/index.js
@@ -83,3 +83,71 @@ $(window).scroll(function(event) {
     } 
   });
 });
+
+$(window).scroll(function(event) {
+  
+  $(".contact").each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in"); 
+    } 
+  });
+});
+
+$(window).scroll(function(event) {
+  
+  $(".booking-form").each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in"); 
+    } 
+  });
+});
+
+
+// EMAIL SECTION
+
+function sendMail() {
+  // getName
+  // getBusinessName
+  // getEmail
+  // getType
+ var name = document.getElementById("name").value;
+ var business = document.getElementById("business").value;
+ var email = document.getElementById("email").value;
+ var dropdown = document.getElementById("dropdown").value;
+ var how = document.getElementById("how").value;
+
+
+ var template_params = {
+   "reply_to": email,
+   "from_name": name,
+   "name": name,
+   "email": email,
+   "talk-to-me": talk-to-me,
+   "how": how
+};
+
+var service_id = "default_service";
+var template_id = "template_1SvvVysN";
+emailjs.send(service_id, template_id, template_params);
+}
+
+function success() {
+
+alert("Your Email Was Successfully Sent!");
+
+
+  //site key 6LeNLqoUAAAAAGOd386OPuSlB8zBSkMdi0UrfTaL
+  //secret key 6LeNLqoUAAAAAFz7HHjHqCAuTdeGNs8JgDKkRKKK
+
+
+}
+
+
+emailjs.send('<YOUR SERVICE ID>', '<YOUR TEMPLATE ID>', templateParams)
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
